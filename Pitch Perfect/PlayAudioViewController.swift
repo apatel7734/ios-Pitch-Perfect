0bb80281879c2ddb9602cf656be1d81f914ac744
Ashish <ashish@a.patel7734@gmail.com>
2015-03-31 11:57:06 -0700

    adding pause feature + echo sound effect.

--- Pitch Perfect/PlayAudioViewController.swift
+++ Pitch Perfect/PlayAudioViewController.swift
@@ -9,40 +9,60 @@
 import UIKit
 import AVFoundation
 
-class PlayAudioViewController: UIViewController{
+class PlayAudioViewController: UIViewController,AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
+    var audioPlayerEcho: AVAudioPlayer!
     var recievedAudio: RecordedAudio!
     
     var audioEngine: AVAudioEngine!
     var audioPlayerNode: AVAudioPlayerNode!
-    var pitchEffect: AVAudioUnitTimePitch!
     var audioFile: AVAudioFile!
+    
+    @IBOutlet weak var playpauseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         /*
         //get the file code structure
         var path = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
         if (path != nil){
         var urlPath = NSURL(fileURLWithPath: path!)
+        
         audioPlayer = AVAudioPlayer(contentsOfURL: urlPath, error: nil)
         audioPlayer.enableRate = true
+        
+        audioPlayerEcho = AVAudioPlayer(contentsOfURL: urlPath, error: nil)
+        audioPlayerEcho.enableRate = true
+        
+        audioFile = AVAudioFile(forReading: urlPath, error: nil)
+        
         }else{
         println("can't find path")
         }
         */
         
+        
+        
         if (recievedAudio.filePathUrl != nil){
-            audioPlayer = AVAudioPlayer(contentsOfURL: recievedAudio.filePathUrl, error: nil)
+            var urlPath = recievedAudio.filePathUrl
+            
+            audioPlayer = AVAudioPlayer(contentsOfURL: urlPath, error: nil)
             audioPlayer.enableRate = true
+            
+            audioPlayerEcho = AVAudioPlayer(contentsOfURL: urlPath, error: nil)
+            audioPlayerEcho.enableRate = true
+            
+            audioFile = AVAudioFile(forReading: urlPath, error: nil)
         }else{
             println("can't find path")
         }
         
+        audioPlayer.delegate=self
         audioEngine = AVAudioEngine()
-        audioFile = AVAudioFile(forReading: recievedAudio.filePathUrl, error: nil)
-
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -52,22 +72,63 @@ class PlayAudioViewController: UIViewController{
     
     
     @IBAction func didSlowButtonClicked(sender: UIButton) {
+        self.stopAllPlayers()
         audioPlayer.rate = 0.5
-        audioPlayer.stop()
         audioPlayer.play()
         
     }
     
     
     @IBAction func didFastButtonClicked(sender: UIButton) {
+        self.stopAllPlayers()
         audioPlayer.rate = 2.0
-        audioPlayer.stop()
         audioPlayer.play()
     }
     
+    
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        println("didFinishPlaying")
+        player.stop()
+        playpauseButton.setImage(UIImage(named: "play"), forState: UIControlState.Normal)
+    }
+    
+    
+    func audioPlayerBeginInterruption(player: AVAudioPlayer!) {
+        player.pause()
+    }
+    
+    func audioPlayerEndInterruption(player: AVAudioPlayer!) {
+        player.play()
+    }
+    
     @IBAction func didStopButtonClicked(sender: UIButton) {
-        audioPlayer.stop()
-        audioPlayerNode.stop()
+        //        audioPlayer.stop()
+        //        audioPlayerNode.stop()
+        
+        
+        /*
+        works for audioPlayer.
+        */
+        if let playingAudio = audioPlayer?.playing{
+            if(playingAudio){
+                audioPlayer.pause()
+                var playImage = UIImage(named: "play")
+                sender.setImage(playImage, forState: UIControlState.Normal)
+            }else{
+                audioPlayer.play()
+                var pauseImage  = UIImage(named: "pause")
+                sender.setImage(pauseImage, forState: UIControlState.Normal)
+                
+            }
+        }
+        
+        /*
+        not sure whats the delegate method for finish playing audioPlayerNode.
+        */
+        if(self.audioPlayerNode != nil){
+            println("audioPlayerNode \(audioPlayerNode.playing)")
+        }
     }
     
     
@@ -77,7 +138,6 @@ class PlayAudioViewController: UIViewController{
         playAudioWithPitch(1000)
         
     }
-    
     
     
     @IBAction func playDarthVaderButtonClicked(sender: UIButton) {
@@ -95,7 +155,7 @@ class PlayAudioViewController: UIViewController{
         audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
         
-        pitchEffect = AVAudioUnitTimePitch()
+        var pitchEffect: AVAudioUnitTimePitch = AVAudioUnitTimePitch()
         pitchEffect.pitch = pitch
         audioEngine.attachNode(pitchEffect)
         
@@ -106,10 +166,33 @@ class PlayAudioViewController: UIViewController{
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         
         audioEngine.startAndReturnError(nil)
-        
         audioPlayerNode.play()
+        
     }
     
+    //playing echo
+    @IBAction func playAudioWithEcho(sender: UIButton){
+        
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0;
+        audioPlayer.play()
+        
+        let delay: NSTimeInterval = 0.2 //100 ms
+        let playTime: NSTimeInterval = audioPlayer.deviceCurrentTime + delay
+        audioPlayerEcho.stop()
+        audioPlayerEcho.currentTime = 0
+        audioPlayerEcho.volume = 0.8
+        audioPlayerEcho.playAtTime(playTime)
+    }
+    
+    
+    func stopAllPlayers(){
+        audioPlayerEcho.stop()
+        audioPlayer.stop()
+        if(audioPlayerNode != nil){
+            audioPlayerNode.stop()
+        }
+    }
     
     
     
@@ -124,3 +207,4 @@ class PlayAudioViewController: UIViewController{
     */
     
 }
+
